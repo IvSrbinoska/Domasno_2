53611aa4cc81604d18bca12e1a9d0f4391b1ac52
Ivana Srbinoska <Ivana Srbinoska@MK_2010>
2018-04-22 18:25:25 +0200

    Add project files.

---
+++ Domasno_2/Controllers/AddressController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domasno_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Domasno_2.Controllers
+{
+    public class AddressController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Index()
+        {
+            //Address adr = new Address
+            //{
+            //    ID = 1,
+            //    FirstName = "John",
+            //    LastName = "Smith",
+            //    Address1 = "23 NW 13th St",
+            //    Address2 = "#134",
+            //    City = "Miami",
+            //    PostalCode = 22431
+            //};
+            ////ViewData["Message"] = adr;
+            //return View(adr);
+
+            var adrress = new List<Address>
+            {
+                new Address
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Address1 = "23 NW 13th St",
+                    Address2 = "#134",
+                    City = "Miami",
+                    PostalCode = 22431 },
+                new Address
+                {
+                    FirstName = "Phil",
+                    LastName = "Jones",
+                    Address1 = "25 NW 20th St",
+                    Address2 = "#200",
+                    City = "Texas",
+                    PostalCode = 34521
+                },
+
+                new Address
+                {
+                    FirstName = "Petar",
+                    LastName = "Petrovski",
+                    Address1 = "Leninova",
+                    Address2 = "#250",
+                    City = "Skopje",
+                    PostalCode = 1000
+                }
+            };
+           // adrress.ForEach(d => context.Movies.Add(d));
+           return View(adrress);
+        }
+    }
+}
---
+++ Domasno_2/Models/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domasno_2.Models
+{
+    public class Address
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        [Display(Name = "Zip")]
+        public int PostalCode { get; set; }
+    }
+}
