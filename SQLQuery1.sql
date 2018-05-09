9a0a816cdffefa84c9dc8bc09332821c4f9c3f5b
Ivana Srbinoska <Ivana Srbinoska@MK_2010>
2018-05-09 21:21:54 +0200

    sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+Select * from Customer where FullName = 'zoki'
+select * from Customer where FullName='Ivana Srbinoska'
+
+exec spCustomerGet
+
+exec spCustumerGetByName @FullName='ivona'
+
+exec spCustomerGetByName @FullName='ivona'
+
+exec spCustomerGetByFullName @FullName='ivona'
